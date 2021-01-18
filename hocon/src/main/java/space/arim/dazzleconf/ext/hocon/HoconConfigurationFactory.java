f5358965f2f7c03b81a0e57c303d43155c01ec55
CDFN <codefun@protonmail.com>
2021-01-18 14:45:34 +0100

    more exception checks

--- hocon/src/main/java/space/arim/dazzleconf/ext/hocon/HoconConfigurationFactory.java
+++ hocon/src/main/java/space/arim/dazzleconf/ext/hocon/HoconConfigurationFactory.java
@@ -2,12 +2,12 @@ package space.arim.dazzleconf.ext.hocon;
 
 import com.typesafe.config.ConfigException;
 import com.typesafe.config.ConfigFactory;
-import com.typesafe.config.ConfigValue;
-import com.typesafe.config.ConfigValueFactory;
 import space.arim.dazzleconf.ConfigurationOptions;
+import space.arim.dazzleconf.error.BadValueException;
 import space.arim.dazzleconf.error.ConfigFormatSyntaxException;
 import space.arim.dazzleconf.error.IllDefinedConfigException;
-import space.arim.dazzleconf.factory.AbstractConfigurationFactory;
+import space.arim.dazzleconf.error.InvalidConfigException;
+import space.arim.dazzleconf.error.MissingValueException;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -15,8 +15,9 @@ import java.io.Writer;
 import java.nio.charset.Charset;
 import java.util.Map;
 import java.util.Objects;
+import space.arim.dazzleconf.factory.HumanReadableConfigurationFactory;
 
-public class HoconConfigurationFactory<C> extends AbstractConfigurationFactory<C> {
+public class HoconConfigurationFactory<C> extends HumanReadableConfigurationFactory<C> {
 
   private final HoconOptions hoconOptions;
 
@@ -36,24 +37,41 @@ public class HoconConfigurationFactory<C> extends AbstractConfigurationFactory<C
   }
 
   @Override
-  protected Charset charset() {
+  public Charset charset() {
     return hoconOptions.charset();
   }
 
   @Override
-  protected Map<String, Object> loadMapFromReader(Reader reader) throws IOException, ConfigFormatSyntaxException {
+  public Map<String, Object> loadMap(Reader reader) throws IOException, InvalidConfigException {
     try {
       return ConfigFactory.parseReader(reader).root().unwrapped();
     } catch (ConfigException e) {
-      if (e.getCause() instanceof ConfigException.Parse) {
+      if (e instanceof ConfigException.BadValue) {
+        throw new BadValueException.Builder().cause(e).build();
+      }
+
+      if (e instanceof ConfigException.IO) {
+        throw new IOException(e);
+      }
+
+      if (e instanceof ConfigException.Null) {
+        throw new BadValueException.Builder().cause(e.getCause()).message(e.getMessage()).build();
+      }
+
+      if (e instanceof ConfigException.Missing){
+        throw MissingValueException.forKeyAndMessage("unknown", e.getMessage());
+      }
+
+      if (e instanceof ConfigException.Parse) {
         throw new ConfigFormatSyntaxException(e);
       }
-      throw new IOException(e);
+
+      throw new IOException("Unknown ConfigException subclass", e);
     }
   }
 
   @Override
-  protected void writeMapToWriter(Map<String, Object> config, Writer writer) throws IOException {
+  public void writeMap(Map<String, Object> config, Writer writer) throws IOException {
     writer.write(ConfigFactory.parseMap(config).root().render());
   }
 }
