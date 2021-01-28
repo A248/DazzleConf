0b47c282797b67bbbc3689a0dab4024fa174bb71
CDFN <codefun@protonmail.com>
2021-01-28 20:58:00 +0100

    final comments support, write and parse using defined options

--- hocon/src/main/java/space/arim/dazzleconf/ext/hocon/HoconConfigurationFactory.java
+++ hocon/src/main/java/space/arim/dazzleconf/ext/hocon/HoconConfigurationFactory.java
@@ -2,19 +2,26 @@ package space.arim.dazzleconf.ext.hocon;
 
 import com.typesafe.config.ConfigException;
 import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigObject;
+import com.typesafe.config.ConfigOrigin;
+import com.typesafe.config.ConfigOriginFactory;
+import com.typesafe.config.ConfigValue;
+import com.typesafe.config.ConfigValueFactory;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import space.arim.dazzleconf.ConfigurationOptions;
 import space.arim.dazzleconf.error.BadValueException;
 import space.arim.dazzleconf.error.ConfigFormatSyntaxException;
 import space.arim.dazzleconf.error.IllDefinedConfigException;
 import space.arim.dazzleconf.error.InvalidConfigException;
 import space.arim.dazzleconf.error.MissingValueException;
-
-import java.io.IOException;
-import java.io.Reader;
-import java.io.Writer;
-import java.nio.charset.Charset;
-import java.util.Map;
-import java.util.Objects;
+import space.arim.dazzleconf.factory.CommentedWrapper;
 import space.arim.dazzleconf.factory.HumanReadableConfigurationFactory;
 
 public class HoconConfigurationFactory<C> extends HumanReadableConfigurationFactory<C> {
@@ -29,9 +36,11 @@ public class HoconConfigurationFactory<C> extends HumanReadableConfigurationFact
    * @param hoconOptions parsing and reading options, can not be null
    * @throws NullPointerException      if {@code configClass} or {@code options} is null
    * @throws IllegalArgumentException  if {@code configClass} is not an interface
-   * @throws IllDefinedConfigException if the configuration entries defined in the config class are invalid
+   * @throws IllDefinedConfigException if the configuration entries defined in the config class are
+   *                                   invalid
    */
-  public HoconConfigurationFactory(Class<C> configClass, ConfigurationOptions options, HoconOptions hoconOptions) {
+  public HoconConfigurationFactory(Class<C> configClass, ConfigurationOptions options,
+      HoconOptions hoconOptions) {
     super(configClass, options);
     this.hoconOptions = Objects.requireNonNull(hoconOptions);
   }
@@ -44,7 +53,7 @@ public class HoconConfigurationFactory<C> extends HumanReadableConfigurationFact
   @Override
   public Map<String, Object> loadMap(Reader reader) throws IOException, InvalidConfigException {
     try {
-      return ConfigFactory.parseReader(reader).root().unwrapped();
+      return ConfigFactory.parseReader(reader, hoconOptions.configParseOptions()).root().unwrapped();
     } catch (ConfigException e) {
       if (e instanceof ConfigException.BadValue) {
         throw new BadValueException.Builder().cause(e).build();
@@ -58,7 +67,7 @@ public class HoconConfigurationFactory<C> extends HumanReadableConfigurationFact
         throw new BadValueException.Builder().cause(e.getCause()).message(e.getMessage()).build();
       }
 
-      if (e instanceof ConfigException.Missing){
+      if (e instanceof ConfigException.Missing) {
         throw MissingValueException.forKeyAndMessage("unknown", e.getMessage());
       }
 
@@ -72,6 +81,43 @@ public class HoconConfigurationFactory<C> extends HumanReadableConfigurationFact
 
   @Override
   public void writeMap(Map<String, Object> config, Writer writer) throws IOException {
-    writer.write(ConfigFactory.parseMap(config).root().render());
+    ConfigObject hoconConfig = convertMapToHocon(config);
+    List<String> commentHeader = getHeader();
+    if (!commentHeader.isEmpty()) {
+      hoconConfig = hoconConfig.withOrigin(hoconConfig.origin().withComments(commentHeader));
+    }
+
+    writer.write(hoconConfig.render(hoconOptions.configRenderOptions()));
+  }
+
+  private ConfigObject convertMapToHocon(Map<String, Object> config) {
+    Map<String, Object> hoconConfigMap = new LinkedHashMap<>();
+    for (Map.Entry<String, Object> entry : config.entrySet()) {
+      hoconConfigMap.put(entry.getKey(), convertValueToHocon(entry.getValue()));
+    }
+
+    return ConfigValueFactory.fromMap(hoconConfigMap);
+  }
+
+  private ConfigValue convertValueToHocon(Object value) {
+    if (value instanceof Map) {
+      @SuppressWarnings("unchecked")
+      Map<String, Object> map = (Map<String, Object>) value;
+
+      return convertMapToHocon(map);
+    }
+    if (value instanceof CommentedWrapper) {
+      CommentedWrapper commentedWrapper = (CommentedWrapper) value;
+      ConfigValue hoconValue = convertValueToHocon(commentedWrapper.getValue());
+
+      return hoconValue.withOrigin(hoconValue.origin().withComments(commentedWrapper.getComments()));
+    }
+
+    return ConfigValueFactory.fromAnyRef(value);
+  }
+
+  @Override
+  public boolean supportsCommentsThroughWrapper() {
+    return true;
   }
 }
