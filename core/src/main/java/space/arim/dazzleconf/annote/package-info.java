b451939300e6fdff38078fc91aa011a95d940dbf
A248 <anandebeh@gmail.com>
2021-02-20 15:22:16 -0500

    Add more and improve existing package-info documentation

--- core/src/main/java/space/arim/dazzleconf/annote/package-info.java
+++ core/src/main/java/space/arim/dazzleconf/annote/package-info.java
@@ -1,19 +1,20 @@
 /**
- * Defines standard annotations to control the behaviour of configuration entries. <br>
+ * Defines annotations which control the behaviour of configuration entries. <br>
  * <br>
- * The most common annotations used will be entry attributes: {@link ConfKey}, {@link ConfComments}, and annotations
- * in {@link ConfDefault} all describe a property of a configuration entry.  {@link SubSection} defines a config entry
+ * The most common annotations used will be entry attributes: {@link space.arim.dazzleconf.annote.ConfKey},
+ * {@link space.arim.dazzleconf.annote.ConfComments}, and annotations in {@link space.arim.dazzleconf.annote.ConfDefault}
+ * all describe a property of a configuration entry.  {@link space.arim.dazzleconf.annote.SubSection} defines a config entry
  * as a nested configuration section. <br>
  * <br>
- * Next are validators. {@link NumericRange} and {@link IntegerRange} specify the range of a numeric type,
- * while {@link CollectionSize} controls the size of some collection. For finer grained validation, {@link ConfValidator}
- * may be specified. <br>
+ * Next are validators. {@link space.arim.dazzleconf.annote.NumericRange} and
+ * {@link space.arim.dazzleconf.annote.IntegerRange} specify the range of a numeric type, while
+ * {@link space.arim.dazzleconf.annote.CollectionSize} controls the size of some collection. For finer grained
+ * validation, {@link space.arim.dazzleconf.annote.ConfValidator} may be specified. <br>
  * <br>
- * There are only 2 annotations applying to a configuration interface rather than individual entries: {@link ConfHeader}
- * and {@link ConfSerialisers}. The former adds a comment header, and the latter attaches additional {@link ValueSerialiser}
+ * There are only 2 annotations applying to a configuration interface rather than individual entries:
+ * {@link space.arim.dazzleconf.annote.ConfHeader} and {@link space.arim.dazzleconf.annote.ConfSerialisers}. The former
+ * adds a comment header, and the latter attaches additional {@link space.arim.dazzleconf.serialiser.ValueSerialiser}
  * implementations used specifically for the target configuration interface.
  * 
  */
 package space.arim.dazzleconf.annote;
-
-import space.arim.dazzleconf.serialiser.ValueSerialiser;
---
+++ core/src/main/java/space/arim/dazzleconf/factory/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides abstract {@link space.arim.dazzleconf.ConfigurationFactory} implementations useful for
+ * those implementing their own format support.
+ *
+ */
+package space.arim.dazzleconf.factory;
