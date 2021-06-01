981351fa64f6a9a38b558da0a1bc85b9811d432a
A248 <theanandbeh@gmail.com>
2021-06-01 12:05:48 -0400

    Fix toolchain set in java 8 IT

---
+++ core/src/it/java8/src/test/java/space/arim/dazzleconf/core/it/java8/IsJava8Test.java
@@ -0,0 +1,36 @@
+/*
+ * DazzleConf
+ * Copyright © 2021 Anand Beh
+ *
+ * DazzleConf is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DazzleConf is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DazzleConf. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU Lesser General Public License.
+ */
+
+package space.arim.dazzleconf.core.it.java8;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledForJreRange;
+import org.junit.jupiter.api.condition.JRE;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class IsJava8Test {
+
+	@Test
+	@EnabledForJreRange(min = JRE.JAVA_9)
+	public void failExceptOnJava8() {
+		fail("Tests must run on Java 8");
+	}
+
+}
